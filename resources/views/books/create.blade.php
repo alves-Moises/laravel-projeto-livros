49e6f8d24d2f21795152eb5e87e0366c53cf567c
Moisés Alves <moisesinho_ra@hotmail.com>
2020-12-25 22:12:30 -0300

    created: crood

---
+++ resources/views/books/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X=UA-Compatible">
+    <title>Books</title>
+</head>
+<body>
+    <form action="">
+        <label for="">Nome do Livro</label><br>
+        <input type="text" name="name"><br>
+        <label for="">Autor</label><br>
+        <input type="text" name="author"><br>
+        <label for="">Data de lançamento</label><br>
+        <input type="date" name="launch"><br>
+        <label for="">status</label><br>
+        <input type="text" name="status"><br>
+        <label for="">Classificação de idade</label><br>
+        <input type="integer" name="classification"><br>
+        <Button>Adicionar Livro</Button>
+        <!-- 'name', 'author', 'launch', 'category', 'status', 'classification'] -->
+    </form>
+</body>
+</html>
